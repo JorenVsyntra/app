f42a88c83a3f167b11138573a66520bdee668130
JorenVsyntra <vandervelden.joren@gmail.com>
2025-01-10 14:48:38 +0100

    login + register page gemaakt

--- abbajo/src/app/components/registerform/registerform.component.spec.ts
+++ abbajo/src/app/components/registerform/registerform.component.spec.ts
@@ -1,23 +1,42 @@
+
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RegisterComponent } from './registerform.component';
 
-import { RegisterformComponent } from './registerform.component';
-
-describe('RegisterformComponent', () => {
-  let component: RegisterformComponent;
-  let fixture: ComponentFixture<RegisterformComponent>;
+describe('RegisterComponent', () => {
+  let component: RegisterComponent;
+  let fixture: ComponentFixture<RegisterComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RegisterformComponent]
+      declarations: [ RegisterComponent ],
+      imports: [ ReactiveFormsModule ]
     })
     .compileComponents();
+  });
 
-    fixture = TestBed.createComponent(RegisterformComponent);
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a valid form', () => {
+    expect(component.registrationForm.valid).toBeFalsy();
+    
+    component.registrationForm.controls['firstName'].setValue('John');
+    component.registrationForm.controls['lastName'].setValue('Doe');
+    component.registrationForm.controls['streetAddress'].setValue('123 Main St');
+    component.registrationForm.controls['city'].setValue('Anytown');
+    component.registrationForm.controls['state'].setValue('California');
+    component.registrationForm.controls['zipCode'].setValue('12345');
+    component.registrationForm.controls['phone'].setValue('(123) 456-7890');
+    component.registrationForm.controls['email'].setValue('john@example.com');
+    
+    expect(component.registrationForm.valid).toBeTruthy();
   });
 });
--- abbajo/src/app/components/registerform/registerform.component.ts
+++ abbajo/src/app/components/registerform/registerform.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-registerform',
-  imports: [],
+  selector: 'app-register',
   templateUrl: './registerform.component.html',
-  styleUrl: './registerform.component.css'
+  styleUrls: ['./registerform.component.css']
 })
-export class RegisterformComponent {
+export class RegisterComponent implements OnInit {
+  registrationForm: FormGroup;
+  states: string[] = [
+    'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut',
+    'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa',
+    'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan',
+    'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+    'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma',
+    'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee',
+    'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
+  ];
 
+  constructor(private fb: FormBuilder) {
+    this.registrationForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      streetAddress: ['', Validators.required],
+      streetAddress2: [''],
+      city: ['', Validators.required],
+      state: ['', Validators.required],
+      zipCode: ['', [Validators.required, Validators.pattern('^[0-9]{5}(?:-[0-9]{4})?$')]],
+      phone: ['', [Validators.required, Validators.pattern('^\\([0-9]{3}\\) [0-9]{3}-[0-9]{4}$')]],
+      email: ['', [Validators.required, Validators.email]],
+      preferences: ['']
+    });
+  }
+  
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    if (this.registrationForm.valid) {
+      console.log(this.registrationForm.value);
+    }
+  }
 }
