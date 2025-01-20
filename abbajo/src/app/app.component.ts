617cd5d865100e1e8074b3e25101478bebd73271
Abdou-4000 <abderrahman.arreffag@cursist.syntrapxl.be>
2025-01-20 19:40:30 +0100

    test

--- abbajo/src/app/app.component.ts
+++ abbajo/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink, RouterOutlet } from '@angular/router';
+//import { ProfilePageComponent } from './components/profilepage/profilepage.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [CommonModule]
+  imports: [CommonModule, RouterOutlet, RouterLink,  ]
 })
 export class AppComponent {
   title = 'abbajo';
-filteredPosts: any;
+//filteredPosts: any;
 }
--- abbajo/src/app/app.routes.ts
+++ abbajo/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './components/registerform/registerform.component';
+import { LoginPageComponent } from './components/loginpage/loginpage.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+
+    { path: 'homepage', component: HomepageComponent},
+    { path: 'loggingin', component: LoginPageComponent },
+    { path: 'registration', component: RegisterComponent},
+    { path: '', redirectTo: '/homepage', pathMatch: 'full'},
+];
