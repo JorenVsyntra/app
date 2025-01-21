b3a2e56f069032bce2d69e7249dd2a9b8c81e9f0
Abdou-4000 <abderrahman.arreffag@cursist.syntrapxl.be>
2025-01-21 11:24:43 +0100

    readded links after merge

--- abbajo/src/app/app.routes.ts
+++ abbajo/src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { RegistrationComponent } from './components/registerform/registerform.component';
 import { LoginPageComponent } from './components/loginpage/loginpage.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
+import { AboutComponent } from './components/about/about.component';
+import { FaqComponent } from './components/faq/faq.component';
 
 
 export const routes: Routes = [
@@ -9,5 +11,7 @@ export const routes: Routes = [
     { path: 'homepage', component: HomepageComponent},
     { path: 'loggingin', component: LoginPageComponent },
     { path: 'registration', component: RegistrationComponent},
+    { path: 'about', component:AboutComponent},
+    { path: 'FAQ', component:FaqComponent},
     { path: '', redirectTo: '/homepage', pathMatch: 'full'},
 ];
